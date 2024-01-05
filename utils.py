d3790437850f5a26909467b1cf93c641f77312d7
gabchaves <gabchaves2@gmail.com>
2024-01-05 11:51:51 -0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,10 @@
+
+from DataSet import df
+
+def format_number(value, prefix = ''):
+    for unit in ['', 'mil']:
+        if value < 1000:
+            return f'{prefix} {value:.2f} {unit}'
+        value /= 1000
+    return f'{prefix} {value:.2f} milhões'
+
